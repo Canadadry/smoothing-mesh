bf3eccaef6399315d4008b5e8bbd2e4ec0704ad2
Mourey jerome <jerome@everycheck.fr>
2020-08-13 11:28:01 +0200

    clean main, allow to call in a loop

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ import {Mesh} from './src/mesh'
 
 
 let scale:number=50
+let steps:number=10;
 let center:Vector = new Vector(400,300)
 let mesh:Mesh;
 
@@ -15,20 +16,22 @@ love.draw = function() {
 
 love.load = ()=>{
 	mesh = new Mesh()
-	//insert one ugly quad
-	mesh.insterQuad([
+
+	let quad = new Quad(0,1,2,3)
+	let points:[Vector,Vector,Vector,Vector] = [
 		new Vector(-1,0).mul(scale).add(center),
 		new Vector(1,0.5).mul(scale).add(center),
 		new Vector(2,1).mul(scale).add(center),
 		new Vector(0,1).mul(scale).add(center),
-	])
+	]
 
-	//insert stretched quad
-	mesh.insterQuad(mesh.quads[0].stretch(mesh.points,scale*4))
+	mesh.insterQuad(points)
 
-	//insert shrinked quad
-	mesh.insterQuad(mesh.quads[1].shrink(mesh.points,0.2))
+	for(let i:number=0;i<steps;i++){
+		points = quad.shrink(quad.stretch(points,scale*4),0.2)
+	}
 
+	mesh.insterQuad(points)
 }
 
 
