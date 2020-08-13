14193ae624ecd6669a5965325ec34297f9c10e07
Mourey jerome <jerome@everycheck.fr>
2020-08-13 11:04:42 +0200

    move code into quad

--- main.ts
+++ main.ts
@@ -6,28 +6,11 @@ import {Mesh} from './src/mesh'
 let scale:number=50
 let center:Vector = new Vector(400,300)
 let mesh:Mesh;
-let diagonals:[Vector,Vector,Vector,Vector]
-let normals:[Vector,Vector,Vector,Vector]
 
 love.draw = function() {
 	love.graphics.clear(0,0,0)
 	mesh.draw()
-	for(let i:number=0;i<4;i++){
-		let p1 = mesh.points[mesh.quads[0].points[i]]
-		let p2 = p1.add(diagonals[i])
 
-		love.graphics.setColor(0,0,1,1)
-		love.graphics.line(p1.x,p1.y,p2.x,p2.y)
-
-	}
-	for(let i:number=0;i<4;i++){
-		let p1 = mesh.points[mesh.quads[0].points[i]]
-		let p2 = p1.add(normals[i].mul(scale))
-
-		love.graphics.setColor(0,1,0,1)
-		love.graphics.line(p1.x,p1.y,p2.x,p2.y)
-
-	}
 }
 
 love.load = ()=>{
@@ -40,35 +23,8 @@ love.load = ()=>{
 		new Vector(0,1).mul(scale).add(center),
 	])
 
-	let q = mesh.quads[0]
-	let p0:Vector = mesh.points[q.points[0]]
-	let p1:Vector = mesh.points[q.points[1]]
-	let p2:Vector = mesh.points[q.points[2]]
-	let p3:Vector = mesh.points[q.points[3]]
-
-
-	let sign:number = (p0.sub(p1).vect(p2.sub(p1))) > 0 ? 1 : -1
-
-	diagonals=[
-		p1.sub(p3),
-		p2.sub(p0),
-		p3.sub(p1),
-		p0.sub(p2),
-	]
-
-	normals = [
-		new Vector(-diagonals[0].y,diagonals[0].x).normalize().mul(sign),
-		new Vector(-diagonals[1].y,diagonals[1].x).normalize().mul(sign),
-		new Vector(-diagonals[2].y,diagonals[2].x).normalize().mul(sign),
-		new Vector(-diagonals[3].y,diagonals[3].x).normalize().mul(sign),
-	]
-
-	mesh.insterQuad([
-		p0.add(normals[0].mul(200)),
-		p1.add(normals[1].mul(200)),
-		p2.add(normals[2].mul(200)),
-		p3.add(normals[3].mul(200)),
-	])
+	//insert stretched quad
+	mesh.insterQuad(mesh.quads[0].stretch(mesh.points,scale*4))
 
 }
 
--- src/quad.ts
+++ src/quad.ts
@@ -24,4 +24,34 @@ export class Quad{
 			points[this.points[0]].x,points[this.points[0]].y
 		)
 	}
+	stretch(points:Vector[],factor:number):[Vector,Vector,Vector,Vector]{
+		let p0:Vector = points[this.points[0]]
+		let p1:Vector = points[this.points[1]]
+		let p2:Vector = points[this.points[2]]
+		let p3:Vector = points[this.points[3]]
+
+
+		let sign:number = (p0.sub(p1).vect(p2.sub(p1))) > 0 ? 1 : -1
+
+		let diagonals:[Vector,Vector,Vector,Vector]=[
+			p1.sub(p3),
+			p2.sub(p0),
+			p3.sub(p1),
+			p0.sub(p2),
+		]
+
+		let normals:[Vector,Vector,Vector,Vector] = [
+			new Vector(-diagonals[0].y,diagonals[0].x).normalize().mul(sign),
+			new Vector(-diagonals[1].y,diagonals[1].x).normalize().mul(sign),
+			new Vector(-diagonals[2].y,diagonals[2].x).normalize().mul(sign),
+			new Vector(-diagonals[3].y,diagonals[3].x).normalize().mul(sign),
+		]
+
+		return [
+			p0.add(normals[0].mul(factor)),
+			p1.add(normals[1].mul(factor)),
+			p2.add(normals[2].mul(factor)),
+			p3.add(normals[3].mul(factor)),
+		]
+	}
 }
